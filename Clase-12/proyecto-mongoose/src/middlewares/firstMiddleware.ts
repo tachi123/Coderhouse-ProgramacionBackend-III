7ef77e0d4b14d6e655bd8a57c71f8ee06aabad58
Jaime Nahuel Ramírez Lorca <jnramirez@seguridadvial.gob.ar>
2025-06-04 07:57:04 -0300

    subo proyecto base

--- Clase-12/proyecto-mongoose/src/middlewares/firstMiddleware.ts
+++ Clase-12/proyecto-mongoose/src/middlewares/firstMiddleware.ts
@@ -5,7 +5,6 @@ import { Request, Response, NextFunction } from 'express';
 export default class FirstMiddleware implements NestMiddleware{
     use(req: Request, res: Response, next: NextFunction){
         console.log(`${req.method} at ${req.url} received`);
-        next()
+        next();
     }
-
 }
